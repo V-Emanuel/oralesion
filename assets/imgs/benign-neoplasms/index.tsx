0106e4e200ecf4875bf0dcb90db3a18645588f43
V-Emanuel <gvictor567@gmail.com>
2026-01-07 12:44:32 -0300

    add: images - benign neoplasms

---
+++ assets/imgs/benign-neoplasms/index.tsx
@@ -0,0 +1,23 @@
+import EpidermoliseBolhosa from './epidermolise_bolhosa.png';
+import EritemaMultiforme from './eritema_multiforme.png';
+import EstomatiteAftosaReciditiva from './estomatite_aftosa_reciditiva.png';
+import GengivostomatiteHerpetica from './gengivostomatite_herpetica.png';
+import HerpesLabialRecorrente from './herpes_labial_recorrente.png';
+import Mucocele from './mucocele.png';
+import PenfigoVulgar from './penfigo_vulgar.png';
+import Penfigoide from './penfigoide.png';
+import Ranula from './ranula.png';
+import Sifilis from './sifilis.png';
+
+export {
+  EpidermoliseBolhosa,
+  EritemaMultiforme,
+  EstomatiteAftosaReciditiva,
+  GengivostomatiteHerpetica,
+  HerpesLabialRecorrente,
+  Mucocele,
+  PenfigoVulgar,
+  Penfigoide,
+  Ranula,
+  Sifilis,
+};
