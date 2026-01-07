81cd49ccbabd6e7baa936948219c1f22a69462ae
V-Emanuel <gvictor567@gmail.com>
2026-01-07 12:36:33 -0300

    add: images - vesiculobullous and ulcerative lesions

---
+++ assets/imgs/vesiculobullous-and-ulcerative-lesions/index.tsx
@@ -0,0 +1,23 @@
+import EpidermoliseBolhosa from "./epidermolise_bolhosa.png";
+import EritemaMultiforme from "./eritema_multiforme.png";
+import EstomatiteAftosa from "./estomatite_aftosa_reciditiva.png";
+import Gengivostomatite from "./gengivostomatite_herpetica.png";
+import HerpesLabial from "./herpes_labial_recorrente.png";
+import Mucocele from "./mucocele.png";
+import Penfigo from "./penfigo_vulgar.png";
+import Penfigoide from "./penfigoide.png";
+import Ranula from "./ranula.png";
+import Sifilis from "./sifilis.png";
+
+export {
+  EpidermoliseBolhosa,
+  EritemaMultiforme,
+  EstomatiteAftosa,
+  Gengivostomatite,
+  HerpesLabial,
+  Mucocele,
+  Penfigo,
+  Penfigoide,
+  Ranula,
+  Sifilis,
+};
