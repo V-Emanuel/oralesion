da54467fc0bd93b517c2de12fd06e89070318a5e
V-Emanuel <gvictor567@gmail.com>
2026-01-07 00:13:02 -0300

    add: images - childhood oral lesions

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,14 +1,3 @@
-declare module "*.png" {
-  const value: any;
-  export default value;
-}
-
-declare module "*.jpg" {
-  const value: any;
-  export default value;
-}
-
-declare module "*.jpeg" {
-  const value: any;
-  export default value;
-}
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
