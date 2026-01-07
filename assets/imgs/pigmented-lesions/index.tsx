c459890d7fb881f0c263efa37439b40e0eb669ec
V-Emanuel <gvictor567@gmail.com>
2026-01-07 12:23:35 -0300

    add: images - white and pigmented lesions

---
+++ assets/imgs/pigmented-lesions/index.tsx
@@ -0,0 +1,21 @@
+import Addison from './addison.png';
+import CandidiaseEritematosa from './candidiase_eritematosa.png';
+import Hemangioma from './hemangoma.png'; // Note que o arquivo está escrito 'hemangoma' na imagem
+import Melanoma from './melanoma.png';
+import NevoMelanocitico from './nevo_melanocitico.png';
+import PeutzJeghers from './peutz_jeghers.png';
+import PigmentacaoRacial from './pigmentacao_racial.png';
+import SarcomaDeKaposi from './sarkoma_de_kaposi.png'; // Note que o arquivo está com 'k' (sarkoma)
+import TatuagemPorAmalgama from './tatuagem_por_amalgama.png';
+
+export {
+  Addison,
+  CandidiaseEritematosa,
+  Hemangioma,
+  Melanoma,
+  NevoMelanocitico,
+  PeutzJeghers,
+  PigmentacaoRacial,
+  SarcomaDeKaposi,
+  TatuagemPorAmalgama,
+};
---
+++ assets/imgs/white-lesions/index.tsx
@@ -0,0 +1,17 @@
+import CandidiasePseudomembranosa from './candidiase_pseudomembranosa.png';
+import GranuloDeFordyce from './granulo_de_fordyce.png';
+import Leucoceratose from './leucoceratose.png';
+import LeucoplasiaPilosa from './leucoplasia_pilosa.png';
+import Leucoplasia from './leucoplasia.png';
+import Liquen from './liquen.png';
+import Queilite from './queilite.png';
+
+export {
+  CandidiasePseudomembranosa,
+  GranuloDeFordyce,
+  Leucoceratose,
+  LeucoplasiaPilosa,
+  Leucoplasia,
+  Liquen,
+  Queilite,
+};
