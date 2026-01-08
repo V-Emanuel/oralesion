27b378ba828ed3b15d1857791e3bc20f14253d54
V-Emanuel <gvictor567@gmail.com>
2026-01-07 23:30:19 -0300

    json: pigmented lesions

--- assets/imgs/pigmented-lesions/index.tsx
+++ assets/imgs/pigmented-lesions/index.tsx
@@ -1,11 +1,11 @@
 import Addison from './addison.png';
 import CandidiaseEritematosa from './candidiase_eritematosa.png';
-import Hemangioma from './hemangoma.png'; // Note que o arquivo está escrito 'hemangoma' na imagem
+import Hemangioma from './hemangoma.png';
 import Melanoma from './melanoma.png';
 import NevoMelanocitico from './nevo_melanocitico.png';
 import PeutzJeghers from './peutz_jeghers.png';
 import PigmentacaoRacial from './pigmentacao_racial.png';
-import SarcomaDeKaposi from './sarkoma_de_kaposi.png'; // Note que o arquivo está com 'k' (sarkoma)
+import SarcomaDeKaposi from './sarkoma_de_kaposi.png';
 import TatuagemPorAmalgama from './tatuagem_por_amalgama.png';
 
 export {
