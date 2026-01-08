f705be17eba26206eea25e222d848b778ea60dff
V-Emanuel <gvictor567@gmail.com>
2026-01-08 00:44:24 -0300

    front page

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Jorge!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/pages/front/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+ 
+export default function Front() {
+    return <Text>Front Page</Text>;
+}
