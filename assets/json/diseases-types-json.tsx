4288a15d620cde9b79cf40436affed4005effc07
V-Emanuel <gvictor567@gmail.com>
2026-01-08 00:01:04 -0300

    add: images on diseases types

--- assets/json/diseases-types-json.tsx
+++ assets/json/diseases-types-json.tsx
@@ -1,43 +1,62 @@
+import { Anquiloglossia } from "../imgs/most-common-oral-lesions";
+import { Candidiase } from "../imgs/childhood-oral-lesions";
+import { Epulide } from "../imgs/non-neoplastic-proliferative-processes";
+import { CandidiasePseudomembranosa } from "../imgs/white-lesions";
+import { CandidiaseEritematosa } from "../imgs/pigmented-lesions";
+import { Adenoma } from "../imgs/malignant-neoplasms";
+import { Condiloma } from "../imgs/benign-neoplasms";
+import { Behcet } from "../imgs/syndromes";
+import { EpidermoliseBolhosa } from "../imgs/vesiculobullous-and-ulcerative-lesions";
+
 export const diseasesTypes = [
     {
-        id: 1,
-        name: "Lesões Mais Comuns",
-    },
-    {
-        id: 2,
-        name: "Doenças Bucais na Infância",
-    },
-     {
-        id: 3,
-        name: "Processos Proliferativos Não Neoplásicos",
-    },
-     {
-        id: 4,
-        name: "Lesões Brancas",
-    },
-     {
-        id: 5,
-        name: "Lesões Pigmentadas",
-    },
-     {
-        id: 6,   
-        name: "Neoplasias Malignas",
-    },
-     {
-        id: 7,
-        name: "Neoplasias Benignas",
-    },
-     {
-        id: 8,
-        name: "Síndromes",
-    },
-    {
-        id: 9,
-        name: "Lesões Vesiculo-bolhosas e Ulcerativas",
-    },
-     {
-        id: 10,
-        name: "Exames complementares",
-    }
-
-]
+    id: 1,
+    name: "Lesões Mais Comuns",
+    img: Anquiloglossia,
+  },
+  {
+    id: 2,
+    name: "Doenças Bucais na Infância",
+    img: Candidiase,
+  },
+  {
+    id: 3,
+    name: "Processos Proliferativos Não Neoplásicos",
+    img: Epulide,
+  },
+  {
+    id: 4,
+    name: "Lesões Brancas",
+    img: CandidiasePseudomembranosa,
+  },
+  {
+    id: 5,
+    name: "Lesões Pigmentadas",
+    img: CandidiaseEritematosa,
+  },
+  {
+    id: 6,
+    name: "Neoplasias Malignas",
+    img: Adenoma,
+  },
+  {
+    id: 7,
+    name: "Neoplasias Benignas",
+    img: Condiloma,
+  },
+  {
+    id: 8,
+    name: "Síndromes",
+    img: Behcet,
+  },
+  {
+    id: 9,
+    name: "Lesões Vesiculo-bolhosas e Ulcerativas",
+    img: EpidermoliseBolhosa,
+  },
+  {
+    id: 10,
+    name: "Exames complementares",
+    img: "",
+  },
+];
