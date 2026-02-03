b3da0230c363fbf83f236f1d351b898f9294db7f
V-Emanuel <gvictor567@gmail.com>
2026-02-03 16:46:06 -0300

    feat: loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,23 @@
 import { Slot } from "expo-router";
+import { useEffect, useState } from "react";
 import Footer from "./components/footer";
+import LoadingPage from "./loadingPage";
 
 export default function Layout() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <>
       <Slot />
